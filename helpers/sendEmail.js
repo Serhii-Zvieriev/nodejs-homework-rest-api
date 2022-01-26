438233335d3dd19c639713ed91daf8ec8b5ab4e1
Serhii-Zvieriev <zacjok@gmail.com>
2022-01-26 21:01:52 +0200

    my emeil write to env

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,14 +1,14 @@
 const sgMail = require("@sendgrid/mail");
 require("dotenv").config();
 
-const { SENDGRID_API_KEY } = process.env;
+const { SENDGRID_API_KEY, MY_EMAIL } = process.env;
 
 sgMail.setApiKey(SENDGRID_API_KEY);
 
 const sendEmail = async (data) => {
   // eslint-disable-next-line no-useless-catch
   try {
-    const email = { ...data, from: "zacjok@gmail.com" };
+    const email = { ...data, from: MY_EMAIL };
     await sgMail.send(email);
     return true;
   } catch (error) {
